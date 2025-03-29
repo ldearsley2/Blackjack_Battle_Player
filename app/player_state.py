6407ebc0ba3d70cdf27d2f4dcda25973e351639c
ldearsley2 <ldearsley2@gmail.com>
2025-03-29 11:22:37 +0000

    Add env variables, add config

--- app/player_state.py
+++ app/player_state.py
@@ -1,14 +1,16 @@
+import os
+
+from app.config import SETTINGS
+
+
 class PlayerState:
     def __init__(self):
-        self.nickname = "Lucas"
+        self.nickname = SETTINGS.player_nickname
         self.player_id = ""
         self.status = ""
 
     def set_player_id(self, player_id: str):
         self.player_id = player_id
-
-    def set_player_bust(self):
-        self.status = "Bust"
 
 
 PLAYER_STATE = PlayerState()
--- app/startup.py
+++ app/startup.py
@@ -3,11 +3,10 @@ import requests
 from app.player_state import PlayerState
 
 
-def start_up_connect(game_url: str, own_url: str, player_state: PlayerState):
-    data = {
-        "url": own_url,
-        "nickname": "Lucas"
-    }
+def start_up_connect(
+    game_url: str, own_url: str, nickname: str, player_state: PlayerState
+):
+    data = {"url": own_url, "nickname": nickname}
     response = requests.post(url=f"{game_url}/connect", json=data)
 
     player_state.set_player_id(response.json()["player_id"])
